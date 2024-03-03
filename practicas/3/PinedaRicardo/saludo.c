562caced65109b4b0d4fee9275ff11b25f82c1e5
Pineda Galindo Ricardo Angel <ricki.pg3@gmail.com>
2024-03-03 15:45:52 -0600

    Agrego saludo personalizado al programa saludo.c

--- practicas/3/PinedaRicardo/saludo.c
+++ practicas/3/PinedaRicardo/saludo.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+char nombre[50];
+
 int main() {
     printf("Hola mundo!!\n");
+    printf("Ingrese su nombre: ");
+    scanf("%[^\n]",&nombre);
+    printf("\nHola %s",nombre);
     return (0);
 }
