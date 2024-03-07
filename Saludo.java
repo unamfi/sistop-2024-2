0e03b963aa167b1338f70e12326088148bcbfa6f
MendozaMartin <lalo_1705@hotmail.com>
2024-03-07 14:07:06 -0600

    Agregando el programa que solo imprime un saludo

--- Saludo.java
+++ Saludo.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 
 public class Saludo{
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Por favor ingresa tu nombre: ");
-        String nombre = scanner.nextLine();
-        System.out.println("Hola " + nombre + "! Bienvenido al programa de saludo.");
-        scanner.close();
+        System.out.println("¡Hola, mundo!");
     }
 }
-
