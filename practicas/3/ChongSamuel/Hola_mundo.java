91521d1657256a16370ce8b6b952a1b5d8d42559
SaChHe <samuel.chong@ingenieria.unam.edu>
2024-03-07 20:57:05 -0600

    Commit de saludo personalizado

--- practicas/3/ChongSamuel/Hola_mundo.java
+++ practicas/3/ChongSamuel/Hola_mundo.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Hola_mundo {
     public static void main(String[] args) {
-        System.out.println("Hola, mundo!");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, introduce tu nombre: ");
+
+        String nombre = scanner.nextLine();
+        System.out.println("¡Hola, " + nombre + "!");
+        scanner.close();
+        
     }
 }
