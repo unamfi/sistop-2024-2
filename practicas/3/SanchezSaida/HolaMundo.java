ff63f79238f41f2da0151d97ec99472f2bcfd6e7
Saida Mayela Sanchez Calvillo <mayelamarshall@gmail.com>
2024-03-07 19:37:00 -0600

    Primer commit: Programa saludo

---
+++ practicas/3/SanchezSaida/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+/**
+ *
+ * @author Saida Sanchez
+ */
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir "Hola Mundo" en la consola
+        System.out.println("Hola Mundo");
+    }
+}
