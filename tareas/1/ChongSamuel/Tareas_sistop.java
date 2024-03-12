51c719ff41413a63c08d4860ac197cf9c63cc0d3
SaChHe <samuel.chong@ingenieria.unam.edu>
2024-03-12 13:20:22 -0600

    Se agregan funciones para inicializar memoria y ver mapa de memoria

--- tareas/1/ChongSamuel/Tareas_sistop.java
+++ tareas/1/ChongSamuel/Tareas_sistop.java
@@ -1,13 +1,36 @@
 package sachhe.tareas_sistop;
-
+import java.util.Scanner;
 /**
  *
  * @author SamChong
  */
 public class Tareas_sistop {
 
+    static char[] memoria;
+    static int tamMemoria = 30;
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
+        Scanner scan = new Scanner(System.in);
+        memoria = new char[tamMemoria];
+        inicializarMemoria();
+        
+       
     }
+    
+    /*Esta funcion inicializa la memoria de 30 unidades, con -*/
+    static void inicializarMemoria(){
+        for(int i = 0; i < tamMemoria; i++){
+            memoria[i] = '-';
+        }
+    }
+    
+    /*Este metodo muestra el estado de la memoria*/
+    static void mapaMemoria(){
+        for(char c: memoria){
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+    
 }
