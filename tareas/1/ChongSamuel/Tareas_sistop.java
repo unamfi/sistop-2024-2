a18f0b86d4f531e97d155f74cf7becb4244425e5
SaChHe <samuel.chong@ingenieria.unam.edu>
2024-03-12 12:56:18 -0600

    Commit inicial, tarea 1

---
+++ tareas/1/ChongSamuel/Tareas_sistop.java
@@ -0,0 +1,13 @@
+package sachhe.tareas_sistop;
+
+/**
+ *
+ * @author SamChong
+ */
+public class Tareas_sistop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
