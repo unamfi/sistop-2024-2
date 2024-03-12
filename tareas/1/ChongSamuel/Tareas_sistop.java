21d6762cd9a5ee486755bb56d41b4509d184d2ec
SaChHe <samuel.chong@ingenieria.unam.edu>
2024-03-12 14:26:00 -0600

    Se corrigen errores, version final

--- tareas/1/ChongSamuel/Tareas_sistop.java
+++ tareas/1/ChongSamuel/Tareas_sistop.java
@@ -9,6 +9,7 @@ public class Tareas_sistop {
     static char[] memoria;
     static int tamMemoria = 30;
     
+    /* Funcion principal del programa */
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
@@ -36,12 +37,14 @@ public class Tareas_sistop {
         }
     }
     
+    /* Este método inicializa la memoria de 30 unidades con - */
     static void inicializarMemoria(){
         for(int i = 0; i < tamMemoria; i++){
             memoria[i] = '-';
         }
     }
     
+    /* Este método muestra el mapa de la memoria */
     static void mapaMemoria(){
         for(char c: memoria){
             System.out.print(c);
@@ -49,20 +52,27 @@ public class Tareas_sistop {
         System.out.println();
     }
     
+    /* Método encargado de compactar la memoria */
     static void compactarMemoria(){
-        char[] memoriaNueva = new char[tamMemoria];
+        char[] memoriaNueva = new char[tamMemoria]; 
         int indice = 0;
+
         for(int i = 0; i < tamMemoria; i++){
             if(memoria[i] != '-'){
-                memoriaNueva[indice] = memoria[i];
+                memoriaNueva[indice] = memoria[i]; //los procesos se van moviendo, compactando la memoria que se refeja del lado derecho
                 indice++;
             }
         }
         memoria = memoriaNueva;
-        System.out.println("Mapa de memoria: ");
+
+        for(int i = indice; i < tamMemoria; i++){
+            memoria[i] = '-';       //Se ve la memoria compactada del lado izquierdo
+        }
+        System.out.println("Mapa actualizado: ");
         mapaMemoria();
     }
     
+    /* Este metodo asigna la memoria a los procesos segun las unidades necesarias; siguiendo el peor ajuste */
     static void asignarMemoria(char proceso){
         Scanner scan = new Scanner(System.in);
         System.out.println("Requiero entre 2 y 15 unidades: ");
@@ -73,13 +83,13 @@ public class Tareas_sistop {
         
         for(int i = 0; i < tamMemoria; i++){
             if(memoria[i] == '-'){
-                if(inicio == -1){
-                    inicio = i;
+                if(inicio == -1){       //Se recorre el arreglo
+                    inicio = i; 
                 }
                 contador++;
                 if(contador == unidadesMemoria){
                     for(int j = inicio; j < inicio + unidadesMemoria; j++){
-                        memoria[j] = proceso;
+                        memoria[j] = proceso;       //se asgina el proceso [a-z]
                     }
                     System.out.println("Nueva asignacion: ");
                     mapaMemoria();
@@ -92,13 +102,15 @@ public class Tareas_sistop {
             }
             System.out.println("Se requiere compactar");
             compactarMemoria();
+            System.out.println("Volver a ingresar las unidades del proceso");
             asignarMemoria(proceso);
      }    
     
+     /* Metodo para liberar memoria */
     static void liberarMemoria(char proceso){
         for(int i = 0; i < tamMemoria; i++){
             if(memoria[i] == proceso){
-                memoria[i] = '-';
+                memoria[i] = '-';       //Segun el proceso que libera memoria, se reemplaza con -
             }
         }
         System.out.println("Mapa actual: ");
