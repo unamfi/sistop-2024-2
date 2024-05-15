44308df46abf6ed61c2f2436f3903ab0c3fa6dc5
Pineda Galindo Ricardo Angel <ricki.pg3@gmail.com>
2024-05-14 22:12:22 -0600

    Corregí errores de varías funciones, mejoré la vista de los archivos y agregué una función de compactación para cuando hay espacio pero está esparcido por los clusters

--- proyectos/1/PinedaRicardo/PinedaGalindoRicardo_proyecto1.py
+++ proyectos/1/PinedaRicardo/PinedaGalindoRicardo_proyecto1.py
@@ -1,25 +1,13 @@
 """
     Creado por Pineda Galindo Ricardo Angel
-    Fecha de creación: 12/05/2024
-    Última Modificación: 13/05/2024
-    Primera modificación: Creación de la función para listar el contenido y otra función para mostrar la 
-    información del sistema
-    Segunda modificación: Reestructuración del código modularizando las funciones, creación de la función
-    para borrar los archivos del sistema de archivos y otra función para copiar archivos del sistema a la computadora
-
-    NOTA: Aún no se implementan hilos, primero debe funcionar bien el programa
-
 """
 from os import path #Se va a utilizar para ver si las rutas dadas existen o no en la computadora y obtener información de los archivos
-from time import localtime,strftime #Esto me sirve para convertir los objetos que contienen las fechas de los archivos de la compu en cadenas
-from operator import itemgetter #Me permite ordenar el diccionario utilizando los valores
-
+from time import localtime 
+from datetime import datetime #Esto me sirve para convertir los objetos que contienen las fechas de los archivos de la compu en cadenas
+from math import ceil #Sirve para calcular el cluster inicial y redondear hacia arriba
+from tabulate import tabulate #Se usa para mostrar los archivos de forma bonita
 #Parte 1: Definición de funciones para poder leer la informacion de la imagen del sistema de archivos
-"""
-    ACLARACIÓN:
-   * Para trabajar con la imagen del sistema de archivos es necesario abrirlo como un archivo binario para poder utilizar la función SEEK,
-     la cual es una pieza fundamental en el proyecto para poder trabajar con bytes específicos dentro del archivo
-"""
+
 def leer_texto(desplazamiento,cantidadBytes):
 #Para trabajar los bytes como cadenas con la codificacion ASCII 8 se utiliza la codificacion Latin-1 que es lo mismo o tambien iso-8859-1
     with open(rutaImagen,'rb') as imagen: #Es una forma eficiente de abrir un archivo, trabajar con él y después cerrarlo
@@ -54,11 +42,11 @@ def leerDirectorio(entradaConInfo,entradaSinInfo,espacioOcupado):
             nombreArchivo = leer_texto(inicio+1,14)   
             tamArchivo = leer_numEntero(inicio+16,4)
             clusterInicial = leer_numEntero(inicio+20,4)
-            horaCreacion = leer_texto(inicio+24,13)
-            horaModificacion = leer_texto(inicio+38,13)
+            fechaCreacion = leer_texto(inicio+24,13)
+            fechaModificacion = leer_texto(inicio+24,13)
             espacioOcupado += tamArchivo
             if nombreArchivo not in infoArchivos.keys():
-                infoArchivos[nombreArchivo] = [tamArchivo,clusterInicial,horaCreacion,horaModificacion,inicio]
+                infoArchivos[nombreArchivo] = [tamArchivo,clusterInicial,fechaCreacion,fechaModificacion,inicio]
             #Se guarda el inicio de la entrada de los archivos para acceder más fácil en el directorio
             entradaConInfo += 1
         else:
@@ -117,21 +105,28 @@ def imprimirMenu():
 #Parte 4: Definición de funciones para poder trabajar con la informacion de la imagen del sistema de archivos
 
 def listarContenido():
+    archivosInfo = []
     print(espacio + "Entradas Ocupadas: " + str(archivos) + "    || Entradas Disponibles: " + str(archivosLibres) )
     print(espacio + "Espacio Disponible: " + str(espacioLibre) + " Bytes  || Espacio Ocupado: " + str(espacioNoLibre) + " Bytes\n\n")
-    for i,llave in enumerate(infoArchivos.keys()):
-        print(espacio + str(i+1) + ". " + llave)
-        print(espacio + ("."*5) + "Tamaño: " + str(infoArchivos[llave][0]) + " bytes")
-        print(espacio + ("."*5) + "Cluster Inicial: " + str(infoArchivos[llave][1]))
-        print(espacio + ("."*5) + "Fecha de creación: " + infoArchivos[llave][2]) #Crear función para imprimir fecha **Posible hilo??
-        print(espacio + ("."*5) + "Fecha de última modificación: " + infoArchivos[llave][3])
+    for llave in infoArchivos.keys():
+        fechaC = datetime.strptime(infoArchivos[llave][2],formatoFecha).strftime(formatoFechaBonita)
+        fechaM = datetime.strptime(infoArchivos[llave][3],formatoFecha).strftime(formatoFechaBonita)
+        archivosInfo.append([llave,str(infoArchivos[llave][0]),str(infoArchivos[llave][1]),fechaC,fechaM])
+    print(tabulate(archivosInfo,headers=["Nombre","Tamaño(Bytes)","Cluster Inicial", "Fecha de Creación", "Fecha de Modificación"],showindex=True,tablefmt="heavy_grid",stralign='center',floatfmt='.6f'))
+        
 
 def copiarArchivoACompu():
+    max = 0
     archivos = list(enumerate(infoArchivos.keys()))
     for i,llave in archivos:
         print(espacio + str(i+1) + ". " + llave)
+    
+    max = len(archivos)
     opcion = int(input("\n\nEscribe el número del archivo que deseas copiar a tu computadora: "))
     opcion -= 1 #Solo fue porque en pantalla mostre los números partiendo del 1
+    if opcion < 0 or opcion > max-1:
+        print("Error 10: La opción seleccionada no pertenece a ningún archivo")
+        return -1
     nombreArchivo = archivos[opcion][1] #Se obtiene el nombre para poder obtener su información del diccionario
     tamArchivo = infoArchivos[nombreArchivo][0]#Se obtiene para saber cuantos bytes va a leer de la imagen
     clusterInicio = infoArchivos[nombreArchivo][1] #Se obtiene para indicar a que byte desplazar el apuntador
@@ -168,24 +163,27 @@ def copiarArchivoASistema():
         nombreArchivo =  rutaArchivo[-1] #Se divide la cadena solo para obtener el nombre que está en la última posición de la lista
         if nombreArchivo.isascii() == False:
             print("Error 8: El nombre del archivo solo puede contener caracteres pertenecientes a ASCII")
+            input("Presiona enter para continuar...")
             return False
         tamNombre = len(nombreArchivo)
         if tamNombre < 14:
             tamNombre = 14 - tamNombre
             nombreArchivo += " " * tamNombre
+        elif tamArchivo > 14:
+            print("Error 9: El nombre del archivo es muy grande, debe ser de máximo 14 caracteres")
+            return -1
         rutaArchivo = chr(92).join(rutaArchivo)
         #Mientras se va asumir que ya está bien el formato
-        horaCreacion = strftime("%Y%m%d%H%M%S",localtime(path.getctime(rutaArchivo)))
-        horaModificacion = strftime("%Y%m%d%H%M%S",localtime(path.getmtime(rutaArchivo)))
+        fechaCreacion = datetime.strftime(datetime.fromtimestamp(path.getctime(rutaArchivo)),formatoFecha)
+        fechaModificacion = datetime.strftime(datetime.fromtimestamp(path.getmtime(rutaArchivo)),formatoFecha)
         #Ya solo falta ver el cluster donde se va a almacenar
-        desplazamientoDatos,clusterInicial = asignacionDeEspacioDatos(tamArchivo)
+        clusterInicial = asignacionDeEspacioDatos(tamArchivo)
         desplazamientoDirectorio = asignacionEspacioDirectorio()
-        escribirEnDirectorio(desplazamientoDirectorio,'-',nombreArchivo,tamArchivo,clusterInicial,horaCreacion,horaModificacion)
-        escribirEnDatos(desplazamientoDatos,tamArchivo,rutaArchivo,0)
+        escribirEnDirectorio(desplazamientoDirectorio,'-',nombreArchivo,tamArchivo,clusterInicial,fechaCreacion,fechaModificacion)
+        escribirEnDatos(clusterInicial*tamCluster,tamArchivo,rutaArchivo,0)
         print("El archivo se copió con éxito!!")
     else:
         return False
-        
 
 def revisarEspacioParaDatos(tamArchivo):
     if archivosLibres == 0:  #Esto sirve para ver el espacio en el directorio
@@ -197,51 +195,76 @@ def revisarEspacioParaDatos(tamArchivo):
     return True #Ya se revisó que si cabe la información del archivo tanto en el directorio como en el espacio de datos
     
 def asignacionDeEspacioDatos(tamArchivo):
+    temp1,temp2 = espacioEntreDatos(tamArchivo) #Es una variable temporal por si no hay espacio, ya no se ocupe después
+    if temp1 != False: #Si hay espacio entre 2 bloques de datos de archivos, entonces lo va a aprovechar el sistema
+        return ceil(temp1/tamCluster)
+    #Como no hay espacio entre archivos, va a buscar antes del primer archivo y después del último
     inicio = 5 * tamCluster
-    #Primero se van a apartar los bytes de los archivos para no revisarlos en vano
-    bytesOcupados = {}
-    for contenido in infoArchivos.values():
-        bytesOcupados[contenido[0]] = contenido[1] #La lista va a guardar el tamaño del archivo y el cluster inicial
     contador = 0 #Sirve para contar los bytes contiguos y ver si se podría guardar la información del archivo
     with open(rutaImagen,'rb') as imagen:
         while True:
             if contador < tamArchivo:
-                if inicio not in bytesOcupados.keys():
-                    imagen.seek(inicio)
-                    if imagen.read(1) == b'\x00':
-                        contador += 1
-                    else:
-                        contador = 0
+                imagen.seek(inicio)
+                if imagen.read(1) == b'\x00':
+                    contador += 1
                 else:
-                    inicio += bytesOcupados[inicio]
+                    contador = 0
+                    if inicio < temp2:
+                        inicio = temp2 #Como el inicio está ocupado, va a revisar después del último archivo
             elif contador == tamArchivo:
                 inicio -= tamArchivo #Con esto se obtiene en byte desde el cual se puede empezar a escribir la información
                 break
             if inicio > espacioTotal: break
             inicio += 1
         imagen.close()
-    clusterInicial = inicio//2048
-    return inicio,clusterInicial
+    if inicio > espacioTotal and espacioLibre >= tamArchivo: compactacion()
+    return ceil(inicio/tamCluster)
+
+def espacioEntreDatos(tamArchivo): #Sirve para revisar si el archivo cabe entre 2 archivos almacenados y ya no buscar otro lugar
+    archivos = list(infoArchivos.values()) 
+    for i in range(len(archivos)-1):
+        despuesDeArchivo1 = archivos[i][1] * tamCluster + archivos[i][0] + 1#Esto me da el byte donde puedo empezar a contar
+        antesdeArchivo2 = archivos[i+1][1] * tamCluster - 1 #Esto me da el byte hasta donde puedo contar
+        if (antesdeArchivo2 - despuesDeArchivo1) >= tamArchivo: #Quiere decir que si cabe la info del nuevo archivo
+            return despuesDeArchivo1,0
+    #En este punto quiere decir que no cupo los datos del nuevo archivo entre los ya existentes
+    return False,archivos[-1][1] * tamCluster + archivos[-1][0]
 
 def asignacionEspacioDirectorio():
     inicio = tamCluster #El directorio empieza en el cluster 1
     with open(rutaImagen,'rb+') as imagen:
         imagen.seek(inicio)
-        caracter = (imagen.read(1)).decode("latin-1")
-        while caracter != '/' and inicio < tamCluster*5:#Esto evita que lea solo del cluster 1 al 4
+        while (imagen.read(1)).decode("latin-1") != '/' and inicio < tamCluster*(numClusters + 1):#Esto evita que lea solo del cluster 1 al 4
             inicio += 64
+            imagen.seek(inicio)
         imagen.close()
     return inicio #Aqui ya encontró una entrada disponible
 
+def compactacion():
+    inicio = numClusters + 1
+    for llave in infoArchivos.keys():
+        with open(rutaImagen,'rb+') as imagen:
+            imagen.seek(inicio*tamCluster)
+            if imagen.read(1) == b'\x00':
+                contenido = getInformacion(inicio*tamCluster,infoArchivos[llave][0],rutaImagen)
+                imagen.write(contenido)
+                escribirEnDirectorio(infoArchivos[llave][4],'-',llave,infoArchivos[llave][0],inicio,
+                                     infoArchivos[llave][2],infoArchivos[llave][3])
+            inicio += infoArchivos[llave][0]
+            inicio = ceil(inicio/tamCluster)
+            imagen.close()
+        
 
 def eliminarArchivo():
-#Para eliminar un archivo hay que borrarlo del directorio y sustituir todo su contenido por los caracteres indicados en las especificaciones
-#del proyecto
     archivos = list(enumerate(infoArchivos.keys()))
     for i,llave in archivos:
         print(espacio + str(i+1) + ". " + llave)
     opcion = int(input("\n\nEscribe el número del archivo que deseas copiar a tu computadora: "))
     opcion -= 1
+    max = len(archivos)
+    if opcion < 0 or opcion > max-1:
+        print("Error 10: La opción seleccionada no pertenece a ningún archivo")
+        return -1
     nombreArchivo = archivos[opcion][1]
     #A continuación se va a sobreescribir toda esa entrada por los valores default de una entrada vacía
     escribirEnDirectorio(infoArchivos[nombreArchivo][4],'/',"##############",0,0,"00000000000000","00000000000000")
@@ -266,6 +289,8 @@ def mostrarInfoSistema():
 #Variables Globales
 
 rutaImagen = "fiunamfs.img"
+formatoFecha = "%Y%m%d%H%M%S"
+formatoFechaBonita = "%Y/%m/%d %H:%M:%S"
 espacio = " " * 5 #Solo se definió para poder imprimir con un espacio antes del contenido
 infoArchivos = {} #Aqui se van a guardar los nombres de los archivos y su tamaño en bytes
 nombreSistema = leer_texto(0,8)
@@ -280,36 +305,37 @@ espacioTotal = 0 #Indica el espacio disponible para guardar datos
 espacioNoLibre = 0
 espacioLibre = 0
 
-#Esto ya no es parte de la funcion, ya es el main
+#MAIN
 archivos,archivosLibres,espacioNoLibre = leerDirectorio(archivos,archivosLibres,espacioNoLibre) #Se debe ir actualizando constantemente
 espacioTotal = tamCluster * (clustersUnidad -5)
 espacioLibre = espacioTotal - espacioNoLibre
-while True:
+menu = '1'
+while menu != '0':
     imprimirMenu()
-    menu = int(input("\n\n"+ espacio +"Escribe la opción a la que deseas acceder: "))
+    menu = input("\n\n"+ espacio +"Escribe la opción a la que deseas acceder: ")
     limpiarPantalla()
-    if menu == 1:
+    if menu == '1':
         listarContenido()
-    elif menu == 2:
+    elif menu == '2':
         copiarArchivoACompu()
-    elif menu == 3:
+    elif menu == '3':
         copiarArchivoASistema()
         archivosLibres = 0
         archivos = 0
         espacioNoLibre = 0
         archivos,archivosLibres,espacioNoLibre = leerDirectorio(archivos,archivosLibres,espacioNoLibre)
         espacioLibre = espacioTotal - espacioNoLibre
-    elif menu == 4:
+    elif menu == '4':
         eliminarArchivo()
         archivosLibres = 0
         archivos = 0
         espacioNoLibre = 0
         archivos,archivosLibres,espacioNoLibre = leerDirectorio(archivos,archivosLibres,espacioNoLibre)
         espacioLibre = espacioTotal - espacioNoLibre
-    elif menu == 5:
+    elif menu == '5':
         if mostrarInfoSistema() is False:
             menu = 0
-    elif menu == 6:
+    elif menu == '6':
         break
     else: 
         print("ERROR 3: Por favor seleccione una de las opciones mostradas en pantalla...")
