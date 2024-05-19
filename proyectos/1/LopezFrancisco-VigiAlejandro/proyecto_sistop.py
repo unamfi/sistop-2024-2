ea288e2000871d6e608184c57fd2f30c4591de28
Daniel Campillo <pacodaniellc_422@comunidad.unam.mx>
2024-05-18 22:06:07 -0600

    Agrego archivo inicial en su ruta correspondiente.

---
+++ proyectos/1/LopezFrancisco-VigiAlejandro/proyecto_sistop.py
@@ -0,0 +1,17 @@
+"""
+Sistema de Archivos para la Facultad de Ingeniería, FiUnamFS
+
+Este programa implementa un sistema de archivos FiUnamFS que permite realizar operaciones como listar, copiar y eliminar archivos. 
+El programa utiliza concurrencia con semáforos para garantizar la integridad de los datos en el sistema de archivos.
+
+Autores:
+- Francisco Daniel López Campillo
+- Marco Alejandro Vigi Garduño
+
+Fecha de creación: 19 de mayo de 2024
+
+Última modificación: 19 de mayo de 2024
+
+Nota: Este programa es parte de un proyecto académico para las últimas dos unidades del curso.
+
+"""
